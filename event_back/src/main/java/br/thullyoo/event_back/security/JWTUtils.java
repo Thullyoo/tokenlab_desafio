a4caac6aa59119228e201d513aa9e1b1bcbe21f7
Thullyoo <thullyofilho@gmail.com>
2025-02-27 16:01:56 -0300

    feat: add method to get user by token

--- event_back/src/main/java/br/thullyoo/event_back/security/JWTUtils.java
+++ event_back/src/main/java/br/thullyoo/event_back/security/JWTUtils.java
@@ -3,24 +3,33 @@ package br.thullyoo.event_back.security;
 import br.thullyoo.event_back.dto.request.auth.LoginRequest;
 import br.thullyoo.event_back.model.User;
 import br.thullyoo.event_back.repository.UserRepository;
+import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jwt.JWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class JWTUtils {
 
     @Autowired
     private JwtEncoder encoder;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public String generateJWT(User user){
 
@@ -37,4 +46,29 @@ public class JWTUtils {
         return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
+    public User getUserByToken(){
+
+        JwtAuthenticationToken jwtAuthenticationToken =
+                (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        if (jwtAuthenticationToken == null) {
+            throw new IllegalStateException("User is not authenticated.");
+        }
+
+        Jwt jwt = jwtAuthenticationToken.getToken();
+        String subject = jwt.getClaim("sub");
+
+        Pattern pattern = Pattern.compile("document='([0-9]+)'");
+        Matcher matcher = pattern.matcher(subject);
+
+        if (matcher.find()){
+            Optional<User> user = userRepository.findByDocument(matcher.group(1));
+            if (user.isEmpty()) {
+                throw new BadCredentialsException("User not found");
+            } else {
+                return user.get();
+            }
+        }
+        throw new IllegalArgumentException("Document not found");
+    }
 }
