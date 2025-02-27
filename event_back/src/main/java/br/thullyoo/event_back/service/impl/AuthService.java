d9587be64c915281e551e483e95bfd29f314b6c7
Thullyoo <thullyofilho@gmail.com>
2025-02-27 15:18:17 -0300

    feat: add Auth Service Interface

---
+++ event_back/src/main/java/br/thullyoo/event_back/service/impl/AuthService.java
@@ -0,0 +1,9 @@
+package br.thullyoo.event_back.service.impl;
+
+import br.thullyoo.event_back.dto.request.auth.LoginRequest;
+import br.thullyoo.event_back.dto.response.auth.TokenResponse;
+
+public interface AuthService {
+
+    public TokenResponse login(LoginRequest loginRequest);
+}
