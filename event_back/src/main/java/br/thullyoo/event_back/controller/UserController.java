fc7cce5950f4082ea9344ab160649f17944dfcd4
Thullyoo <thullyofilho@gmail.com>
2025-02-27 14:32:19 -0300

    feat: add UserController with endpoint to register and update user

---
+++ event_back/src/main/java/br/thullyoo/event_back/controller/UserController.java
@@ -0,0 +1,33 @@
+package br.thullyoo.event_back.controller;
+
+import br.thullyoo.event_back.dto.request.user.UserRegisterRequest;
+import br.thullyoo.event_back.dto.request.user.UserUpdateRequest;
+import br.thullyoo.event_back.model.User;
+import br.thullyoo.event_back.service.impl.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> registerUser(@RequestBody @Valid UserRegisterRequest userRequest){
+        User user = userService.registerUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PutMapping(value = "/update/{document}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateUser(@PathVariable("document") String document, @RequestBody @Valid
+                                            UserUpdateRequest userUpdateRequest){
+        userService.updateUser(document, userUpdateRequest);
+        return ResponseEntity.ok().body(null);
+    }
+}
