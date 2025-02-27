11949eb13a67a45a6a7edb610f81be80e0ecaaac
Thullyoo <thullyofilho@gmail.com>
2025-02-27 15:32:01 -0300

    feat: add verification for encryption on login

--- event_back/src/main/java/br/thullyoo/event_back/security/JWTUtils.java
+++ event_back/src/main/java/br/thullyoo/event_back/security/JWTUtils.java
@@ -20,25 +20,12 @@ import java.util.Optional;
 public class JWTUtils {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
     private JwtEncoder encoder;
 
-    public String generateJWT(LoginRequest loginRequest){
+    public String generateJWT(User user){
 
         final Instant issuedAt = Instant.now();
         final Instant expiresAt = issuedAt.plusSeconds(300);
-
-        Optional<User> user = userRepository.findByDocument(loginRequest.document());
-
-        if (user.isEmpty()){
-            throw new BadCredentialsException("User not found");
-        }
-
-        if (!Objects.equals(user.get().getPassword(), loginRequest.password())){
-            throw new BadCredentialsException("Document or password incorrect");
-        }
 
         var claims = JwtClaimsSet.builder()
                 .issuer("JWTUtils")
--- event_back/src/main/java/br/thullyoo/event_back/service/AuthServiceImpl.java
+++ event_back/src/main/java/br/thullyoo/event_back/service/AuthServiceImpl.java
@@ -2,10 +2,17 @@ package br.thullyoo.event_back.service;
 
 import br.thullyoo.event_back.dto.request.auth.LoginRequest;
 import br.thullyoo.event_back.dto.response.auth.TokenResponse;
+import br.thullyoo.event_back.model.User;
+import br.thullyoo.event_back.repository.UserRepository;
 import br.thullyoo.event_back.security.JWTUtils;
 import br.thullyoo.event_back.service.impl.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -13,8 +20,25 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     private JWTUtils jwtUtils;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public TokenResponse login(LoginRequest loginRequest) {
-        String token = jwtUtils.generateJWT(loginRequest);
+
+        Optional<User> user = userRepository.findByDocument(loginRequest.document());
+
+        if (user.isEmpty()){
+            throw new BadCredentialsException("User not found");
+        }
+
+        if (!passwordEncoder.matches(loginRequest.password(), user.get().getPassword())){
+            throw new BadCredentialsException("Document or password incorrect");
+        }
+
+        String token = jwtUtils.generateJWT(user.get());
         Long expiresAt = 300L;
         return new TokenResponse(token, expiresAt);
     }
