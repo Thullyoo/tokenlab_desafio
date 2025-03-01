730399d4dd904592372455cf4a08578f159f4848
Thullyoo <thullyofilho@gmail.com>
2025-03-01 15:55:22 -0300

    feat: add annotation for notblank description

--- event_back/src/main/java/br/thullyoo/event_back/dto/request/event/EventRequest.java
+++ event_back/src/main/java/br/thullyoo/event_back/dto/request/event/EventRequest.java
@@ -1,11 +1,15 @@
 package br.thullyoo.event_back.dto.request.event;
 
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-public record EventRequest(String description,
+public record EventRequest(
+                            @NotBlank(message = "Description cannot be blank")
+                            String description,
                            @NotNull(message = "The date and time cannot be null")
                            @FutureOrPresent(message = "The date and time must be in the present or future")
                            LocalDateTime startTime,
