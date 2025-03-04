f0a73d1c04d7168041d9e5615ee51b8b0818e19e
Thullyoo <thullyofilho@gmail.com>
2025-03-04 17:27:29 -0300

    feat: add Invite model

---
+++ event_back/src/main/java/br/thullyoo/event_back/model/Invite.java
@@ -0,0 +1,65 @@
+package br.thullyoo.event_back.model;
+
+import jakarta.persistence.*;
+
+@Table(name = "TB_INVITES")
+@Entity
+public class Invite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    private boolean viewed;
+
+    private boolean accepted;
+
+    public Invite() {
+        this.viewed = false;
+        this.accepted = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+}
