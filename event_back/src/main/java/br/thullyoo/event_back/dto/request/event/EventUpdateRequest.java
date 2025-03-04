e5622b9c3f4cdf1431d7df539f77e4bb6870d372
Thullyoo <thullyofilho@gmail.com>
2025-03-04 17:25:58 -0300

    feat: remove eventId for the EventUpdateRequest

--- event_back/src/main/java/br/thullyoo/event_back/dto/request/event/EventUpdateRequest.java
+++ event_back/src/main/java/br/thullyoo/event_back/dto/request/event/EventUpdateRequest.java
@@ -1,7 +1,6 @@
 package br.thullyoo.event_back.dto.request.event;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-public record EventUpdateRequest(Long eventId, String description, LocalDateTime startTime, LocalDateTime endTime) {
+public record EventUpdateRequest(String description, LocalDateTime startTime, LocalDateTime endTime) {
 }
