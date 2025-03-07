59911050457f74a9f138683d304155fac4949e5c
Thullyoo <thullyofilho@gmail.com>
2025-03-07 15:23:51 -0300

    feat: add inviteId on InviteResponse

--- event_back/src/main/java/br/thullyoo/event_back/dto/response/invite/InviteResponse.java
+++ event_back/src/main/java/br/thullyoo/event_back/dto/response/invite/InviteResponse.java
@@ -1,4 +1,4 @@
 package br.thullyoo.event_back.dto.response.invite;
 
-public record InviteResponse(String userSender, Long eventId, boolean isViewed) {
+public record InviteResponse(Long inviteId, String userSender, Long eventId) {
 }
