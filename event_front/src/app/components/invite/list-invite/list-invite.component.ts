0cf69b68804d1c42fc37936e7fcdbfbaf55f9687
Thullyoo <thullyofilho@gmail.com>
2025-03-10 11:50:41 -0300

    feat: add page to list invites

---
+++ event_front/src/app/components/invite/list-invite/list-invite.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject, type OnInit } from '@angular/core';
+import type { InviteResponse } from '../../../model/invite/InviteResponse';
+import { InviteService } from '../../../services/invite/invite.service';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-list-invite',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './list-invite.component.html',
+  styleUrl: './list-invite.component.scss'
+})
+export class ListInviteComponent implements OnInit {
+
+  private inviteService = inject(InviteService);
+
+  listInvite : InviteResponse[] = []
+
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.inviteService.getInviteByUser().subscribe({
+      next: (res) => {
+        this.listInvite = res;
+      },
+      error: (err) =>{
+        console.error(err);
+      }
+    })
+  }
+  
+  acceptInvite(inviteId: number) {
+    this.inviteService.acceptedInvite({
+      isAccepted: true,
+      inviteId : inviteId
+    })
+    this.router.navigateByUrl("/list-events");
+  }
+  declineInvite(inviteId: number) {
+    this.inviteService.acceptedInvite({
+      isAccepted: true,
+      inviteId : inviteId
+    });
+    window.location.reload();
+  }
+}
