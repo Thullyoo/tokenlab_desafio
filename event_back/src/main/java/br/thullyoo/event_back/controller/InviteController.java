938d35136abbd5118bfd0837a4085c1ebf3c6c8d
Thullyoo <thullyofilho@gmail.com>
2025-03-10 11:36:21 -0300

    feat: add endpoint to get inviteByUser and acceptedInvite

--- event_back/src/main/java/br/thullyoo/event_back/controller/InviteController.java
+++ event_back/src/main/java/br/thullyoo/event_back/controller/InviteController.java
@@ -1,16 +1,16 @@
 package br.thullyoo.event_back.controller;
 
 import br.thullyoo.event_back.dto.request.invite.InviteRequest;
+import br.thullyoo.event_back.dto.request.invite.IsAcceptedInvite;
+import br.thullyoo.event_back.dto.response.invite.InviteResponse;
 import br.thullyoo.event_back.model.Invite;
 import br.thullyoo.event_back.service.impl.InviteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/invite")
@@ -20,11 +20,20 @@ public class InviteController {
     private InviteServiceImpl inviteService;
 
     @PostMapping(value = "/register")
-    public ResponseEntity<Invite> registerEnvite(@RequestBody InviteRequest inviteRequest){
+    public ResponseEntity<Invite> registerInvite(@RequestBody InviteRequest inviteRequest){
         Invite invite = inviteService.registerInvite(inviteRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(invite);
     }
 
+    @GetMapping(value = "/list-invite")
+    public ResponseEntity<List<InviteResponse>> listInviteByUser(){
+        List<InviteResponse> inviteList = inviteService.getInviteByUser();
+        return ResponseEntity.status(HttpStatus.OK).body(inviteList);
+    }
 
-
+    @PostMapping(value = "/isAccepted")
+    public ResponseEntity<Void> isAcceptedInvite(@RequestBody IsAcceptedInvite isAcceptedInvite){
+        inviteService.isAcceptedInvite(isAcceptedInvite);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
 }
