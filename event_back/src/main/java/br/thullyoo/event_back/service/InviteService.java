3803ca42f3245944ca6d48aaeb47caf1e2287f50
Thullyoo <thullyofilho@gmail.com>
2025-03-10 11:35:36 -0300

    feat: add method to get inviteByUser and acceptedInvite

--- event_back/src/main/java/br/thullyoo/event_back/service/InviteService.java
+++ event_back/src/main/java/br/thullyoo/event_back/service/InviteService.java
@@ -1,10 +1,18 @@
 package br.thullyoo.event_back.service;
 
 import br.thullyoo.event_back.dto.request.invite.InviteRequest;
+import br.thullyoo.event_back.dto.request.invite.IsAcceptedInvite;
+import br.thullyoo.event_back.dto.response.invite.InviteResponse;
 import br.thullyoo.event_back.model.Invite;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface InviteService {
 
     public Invite registerInvite(InviteRequest inviteRequest);
 
+    public List<InviteResponse> getInviteByUser();
+
+    public void isAcceptedInvite(IsAcceptedInvite isAcceptedInvite);
 }
--- event_back/src/main/java/br/thullyoo/event_back/service/impl/InviteServiceImpl.java
+++ event_back/src/main/java/br/thullyoo/event_back/service/impl/InviteServiceImpl.java
@@ -1,6 +1,8 @@
 package br.thullyoo.event_back.service.impl;
 
 import br.thullyoo.event_back.dto.request.invite.InviteRequest;
+import br.thullyoo.event_back.dto.request.invite.IsAcceptedInvite;
+import br.thullyoo.event_back.dto.response.invite.InviteResponse;
 import br.thullyoo.event_back.model.Event;
 import br.thullyoo.event_back.model.Invite;
 import br.thullyoo.event_back.model.User;
@@ -13,8 +15,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.*;
 
 @Service
 public class InviteServiceImpl implements InviteService {
@@ -57,4 +58,45 @@ public class InviteServiceImpl implements InviteService {
         return invite;
     }
 
+    public List<InviteResponse> getInviteByUser(){
+
+        User user = jwtUtils.getUserByToken();
+
+        List<InviteResponse> inviteResponseList = new ArrayList<>();
+
+        for(Invite invite : user.getReceivedInvites()){
+            if (!invite.isViewed()){
+                inviteResponseList.add(new InviteResponse(invite.getId(), invite.getSender().getName(), invite.getEvent().getId()));
+            }
+        }
+        return inviteResponseList;
+    }
+
+    public void isAcceptedInvite(IsAcceptedInvite isAcceptedInvite) {
+        User user = jwtUtils.getUserByToken();
+
+        Invite invite = inviteRepository.findById(isAcceptedInvite.inviteId())
+                .orElseThrow(() -> new IllegalArgumentException("Invite not found"));
+
+        if (!invite.getReceiver().equals(user)) {
+            throw new IllegalArgumentException("This invitation was not sent to the logged user");
+        }
+
+        invite.setViewed(true);
+
+        if (isAcceptedInvite.isAccepted()) {
+            if (invite.getEvent().getMembers().contains(user)) {
+                throw new IllegalArgumentException("The user is already a member of this event");
+            }
+
+            Set<User> members = invite.getEvent().getMembers();
+            members.add(user);
+            invite.getEvent().setMembers(members);
+            invite.setAccepted(true);
+            eventRepository.save(invite.getEvent());
+        }
+
+        inviteRepository.save(invite);
+        userRepository.save(user);
+    }
 }
