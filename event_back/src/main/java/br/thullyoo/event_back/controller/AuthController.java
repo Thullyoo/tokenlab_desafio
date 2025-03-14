209110d63dfae1cbc8af13fdf6b00f646282933e
Thullyoo <thullyofilho@gmail.com>
2025-03-14 18:05:09 -0300

    feat: add swagger annotation

--- event_back/src/main/java/br/thullyoo/event_back/controller/AuthController.java
+++ event_back/src/main/java/br/thullyoo/event_back/controller/AuthController.java
@@ -3,6 +3,10 @@ package br.thullyoo.event_back.controller;
 import br.thullyoo.event_back.dto.request.auth.LoginRequest;
 import br.thullyoo.event_back.dto.response.auth.TokenResponse;
 import br.thullyoo.event_back.service.impl.AuthServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Authentication", description = "APIs for user authentication")
 public class AuthController {
 
     @Autowired
     private AuthServiceImpl authService;
 
+    @Operation(summary = "User login", description = "Authenticate a user and return a JWT token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successful, JWT token returned"),
+            @ApiResponse(responseCode = "401", description = "Invalid credentials")
+    })
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
         TokenResponse tokenResponse = authService.login(loginRequest);
         return ResponseEntity.ok().body(tokenResponse);
     }
