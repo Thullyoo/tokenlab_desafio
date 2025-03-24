217e26a1c46b31a9b36754b7999121f8d62eb079
Thullyoo <thullyofilho@gmail.com>
2025-03-24 17:48:08 -0300

    feat: remove get token by service on request

--- event_front/src/app/services/event/event.service.ts
+++ event_front/src/app/services/event/event.service.ts
@@ -2,7 +2,6 @@ import { EventRequest } from './../../model/event/EventRequest';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth/auth.service';
 import type { EventResponse } from '../../model/event/EventResponse';
 import type { Observable } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
@@ -18,19 +17,10 @@ export class EventService {
 
   private url = "http://localhost:8080/api/event";
 
-  authService = inject(AuthService);
-
   toastService = inject(ToastrService);
 
   registerEvent(eventRequest: EventRequest){
-
-    const token = this.authService.getToken()
-
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-    });
-    
-    this.httpClient.post(this.url + "/register", eventRequest, {headers}).subscribe({
+    this.httpClient.post(this.url + "/register", eventRequest).subscribe({
       next: () =>  {
         this.toastService.success("Evento registrado com sucesso!");
         this.router.navigateByUrl("event-list")
@@ -43,47 +33,22 @@ export class EventService {
   }
 
   getEvents(): Observable<EventResponse[]> {
-    const token = this.authService.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-    });
-  
-    return this.httpClient.get<EventResponse[]>(this.url + "/list-events", { headers });
+
+    return this.httpClient.get<EventResponse[]>(this.url + "/list-events");
   }
   getEventById(id : number) : Observable<EventResponse>{
-    const token = this.authService.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`, 
-    });
-    
-    return this.httpClient.get<EventResponse>(this.url + `/${id}`, { headers });
+    return this.httpClient.get<EventResponse>(this.url + `/${id}`);
   }
 
   isEventCreator(id: number): Observable<boolean>{
-    const token = this.authService.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`, 
-    });
-    
-    return this.httpClient.get<boolean>(this.url + `/event-creator/${id}`, { headers });
+    return this.httpClient.get<boolean>(this.url + `/event-creator/${id}`);
   }
 
   deleteEvent(id : number){
-    const token = this.authService.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`, 
-    });
-    
-    return this.httpClient.delete(this.url + `/delete/${id}`, { headers });
+    return this.httpClient.delete(this.url + `/delete/${id}`);
   }
 
   updateEvent(id:number, event : EventRequest){
-
-    const token = this.authService.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`, 
-    });
-    
-    return this.httpClient.put(this.url + `/update/${id}`, event, { headers }).subscribe();
+    return this.httpClient.put(this.url + `/update/${id}`, event).subscribe();
   }
 }
--- event_front/src/app/services/invite/invite.service.ts
+++ event_front/src/app/services/invite/invite.service.ts
@@ -1,7 +1,6 @@
 import { isAccepeted } from './../../model/invite/InviteAccepted';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
 import type { InviteRequest } from '../../model/invite/InviteRequest';
 import { InviteResponse } from '../../model/invite/InviteResponse';
 
@@ -13,36 +12,15 @@ export class InviteService {
 
   private url = "http://localhost:8080/api/invite";
 
-  authService = inject(AuthService);
-
   registerInvite(inviteRequest: InviteRequest){
-
-    const token = this.authService.getToken()
-
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-    });
-
-    return this.httpClient.post(this.url + "/register", inviteRequest, {headers}).subscribe();
+    return this.httpClient.post(this.url + "/register", inviteRequest).subscribe();
   }
 
   getInviteByUser(){
-    const token = this.authService.getToken()
-
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-    });
-
-    return this.httpClient.get<InviteResponse[]>(this.url + "/list-invite", {headers});
+    return this.httpClient.get<InviteResponse[]>(this.url + "/list-invite");
   }
 
   acceptedInvite(isAccepeted : isAccepeted){
-    const token = this.authService.getToken()
-
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-    });
-
-    return this.httpClient.post(this.url + "/isAccepted", isAccepeted,  {headers}).subscribe();
+    return this.httpClient.post(this.url + "/isAccepted", isAccepeted).subscribe();
   }
 }
